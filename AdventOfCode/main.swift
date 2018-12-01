23576e0e1fb672325c1d529540a4d70159f83c5c
Mark Vader <mvader@advancedmd.com>
2018-12-01 15:20:10 -0700

    Initial Commit

---
+++ AdventOfCode/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AdventOfCode
+//
+//  Created by Mark Vader on 12/1/18.
+//  Copyright © 2018 NorthPole. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
